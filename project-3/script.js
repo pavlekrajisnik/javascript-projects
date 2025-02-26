388fa6657af0242392e7e4456b6dd47a6dd61a79
pavlekrajisnik <pavle@beonweb.io>
2025-02-26 20:28:20 +0100

    Finished reset has added

--- project-3/script.js
+++ project-3/script.js
@@ -21,25 +21,39 @@ const switchPlayer = function () {
   player0.classList.toggle('player--active');
   player1.classList.toggle('player--active');
 };
+const init = function () {
+  playing = true;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  diceEl.classList.add('hidden');
+  score0El.innerHTML = 0;
+  score1El.innerHTML = 0;
+  currentScore0.innerHTML = 0;
+  currentScore1.innerHTML = 0;
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+};
 
 //Conditions
-let playing = true;
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let playing;
+let scores;
+let currentScore;
+let activePlayer;
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+init();
 
 // Implementing roll dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
     const diceScore = Math.trunc(Math.random() * 6) + 1;
     console.log(diceScore);
-
     //removed only before first rolling dice
     diceEl.classList.remove('hidden');
-
     diceEl.src = `assets/dice-${diceScore}.png`;
     if (diceScore !== 1) {
       currentScore += diceScore;
@@ -70,3 +84,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
