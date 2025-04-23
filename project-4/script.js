70f7d1e5e1eeea38d0fb614deeb53615673022ac
pavlekrajisnik <pavle@beonweb.io>
2025-04-23 16:24:19 +0200

    created username

--- project-4/script.js
+++ project-4/script.js
@@ -79,3 +79,16 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const createUserName = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUserName(accounts);
+console.log(accounts);
