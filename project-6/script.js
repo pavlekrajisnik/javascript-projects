54dd590342bf99496b27f0353e0116554fc724d3
pavlekrajisnik <pavle@beonweb.io>
2025-06-02 22:21:41 +0200

    add map marker

--- project-6/script.js
+++ project-6/script.js
@@ -16,7 +16,6 @@ if(navigator.geolocation){
   navigator.geolocation.getCurrentPosition(function(position){
     const {latitude} = position.coords;
     const {longitude} = position.coords;
-    console.log(latitude,longitude);
     const coords = [latitude,longitude];
     var map = L.map('map').setView(coords, 13);
 
@@ -24,9 +23,19 @@ if(navigator.geolocation){
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        map.on("click",function(mapEvent){
+        console.log(mapEvent);
+        const {lat,lng} = mapEvent.latlng;
+
+        L.marker([lat,lng]).addTo(map)
+        .bindPopup(L.popup({
+          maxWidth:250,
+          minWidth:100,
+          autoClose:false,
+          className:'running-popup',
+        })).setPopupContent("Workout")
         .openPopup();
+    })
   },function(){
     alert("Could not get your location");
   });
