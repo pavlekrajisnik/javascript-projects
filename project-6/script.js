5445f33ebe9e87ea8b2a4ec8db498fee2b84d6f8
pavlekrajisnik <pavle@beonweb.io>
2025-06-03 15:44:33 +0200

    implementing class,regular function call(return undefined) have to use bind(this), addEventListener(callback function- method without () )

--- project-6/script.js
+++ project-6/script.js
@@ -11,43 +11,61 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map;
-let mapEvent;
+let map ,mapEvent;
 
-if(navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(function(position){
-    const {latitude} = position.coords;
-    const {longitude} = position.coords;
-    const coords = [latitude,longitude];
-    map = L.map('map').setView(coords, 13);
+class App {
+  #map;
+  #mapEvent;
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  constructor(){
+    this._getPosition();
+    form.addEventListener("submit",this._newWorkout.bind(this));
+    inputType.addEventListener('change',this._toggleElevationField);
+  }
+  
+  _getPosition(){
+     if(navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),function(){
+          alert("Could not get your location");
+      });
+    }
+
+  _loadMap(position){
+      const {latitude} = position.coords;
+      const {longitude} = position.coords;
+      const coords = [latitude,longitude];
+      this.#map = L.map('map').setView(coords, 13);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+      }).addTo(this.#map);
+      this.#map.on("click",this._showForm.bind(this));
+  }   
 
-        map.on("click",function(mapE){
-        mapEvent = mapE;
-        form.classList.remove("hidden");
-        inputDistance.focus();
-    })
-  },function(){
-    alert("Could not get your location");
-  });
+  _showForm(mapE){
+      this.#mapEvent = mapE;
+      form.classList.remove("hidden");
+      inputDistance.focus();
+  }
+
+  _toggleElevationField(){
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(e){
+    e.preventDefault();
+     inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+     inputDistance.focus();
+    const {lat,lng} = this.#mapEvent.latlng;
+    L.marker([lat,lng]).addTo(this.#map)
+    .bindPopup(L.popup({
+      maxWidth:250,
+      minWidth:100,
+      autoClose:false,
+      className:'running-popup',
+    })).setPopupContent("Workout")
+    .openPopup();
+  }
 }
-form.addEventListener("submit",function(e){
-  e.preventDefault();
-   inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
-     const {lat,lng} = mapEvent.latlng;
-        L.marker([lat,lng]).addTo(map)
-        .bindPopup(L.popup({
-          maxWidth:250,
-          minWidth:100,
-          autoClose:false,
-          className:'running-popup',
-        })).setPopupContent("Workout")
-        .openPopup();
-})
-inputType.addEventListener('change',function(){
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-})
+  const app = new App();
+  // app._getPosition(); Mogu ovako pozvati ovu metodu
