63b938aa74ea2552980d95c651cf359d93479ef9
pavlekrajisnik <pavle@beonweb.io>
2025-06-03 15:47:43 +0200

    Commit

--- project-6/script.js
+++ project-6/script.js
@@ -11,7 +11,6 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map ,mapEvent;
 
 class App {
   #map;
