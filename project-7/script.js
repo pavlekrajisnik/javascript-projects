5844769c384cb03d344940db70d7d66748a75903
pavlekrajisnik <pavle@beonweb.io>
2025-07-23 15:12:50 +0200

    XMLhttprequest callback hell

--- project-7/script.js
+++ project-7/script.js
@@ -10,15 +10,9 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
-const getCountryData = function(country){
-const request = new XMLHttpRequest();
-request.open("GET",`https://restcountries.com/v2/name/${country}`);
-request.send();
-request.addEventListener("load",function(){
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-  const html = 
-        `<article class="country">
+const renderCountry = function(data,className = ''){
+   const html = 
+        `<article class="country ${className}">
           <img class="country__img" src="${data.flag}" />
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -31,6 +25,25 @@ request.addEventListener("load",function(){
         `;
         countriesContainer.insertAdjacentHTML("beforeend",html);
         countriesContainer.style.opacity = 1;
-});
 }
-getCountryData('Serbia');
+
+const getCountryAndNeighbour= function(country){
+  const request = new XMLHttpRequest();
+  request.open("GET",`https://restcountries.com/v2/name/${country}`);
+  request.send();
+  request.addEventListener("load",function(){
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+    const neighbour = data.borders?.[0];
+    if(!neighbour) return;
+    const request2 = new XMLHttpRequest();
+      request2.open("GET",`https://restcountries.com/v2/alpha/${neighbour}`);
+      request2.send();
+      request2.addEventListener("load",function(){
+        const data2 = JSON.parse(this.responseText);
+        renderCountry(data2,"neighbour");
+         });
+  });
+}
+getCountryAndNeighbour('Serbia');
