11d26bc1a9a0ab42f5ae0d8dfb22ce92f086ef23
pavlekrajisnik <pavle@beonweb.io>
2025-08-07 19:50:10 +0200

    refactoring

--- final_project/src/js/model.js
+++ final_project/src/js/model.js
@@ -18,7 +18,6 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(state.recipe);
   }catch(err){
     alert(err);
   }
