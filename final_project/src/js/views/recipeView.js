ead16ea08b5e5fcbe4ce173d797568b6feed6a0e
pavlekrajisnik <pavle@beonweb.io>
2025-09-10 11:55:47 +0200

    commit

--- final_project/src/js/views/recipeView.js
+++ final_project/src/js/views/recipeView.js
@@ -21,7 +21,7 @@ export class RecipeView extends View {
   _generateMarkup(){
     return ` 
       <figure class="recipe__fig">
-          <img src="${this._data.image}" alt="${this._data.title}" class="recipe__img" />
+         u <img src="${this._data.image}" alt="${this._data.title}" class="recipe__img" />
           <h1 class="recipe__title">
             <span>${this._data.title}</span>
           </h1>
