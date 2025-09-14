61a22cd64a1973a7e72192254b0b47057d5d9889
pavlekrajisnik <pavle@beonweb.io>
2025-09-14 21:35:56 +0200

    add bookmarks to localstorage

--- final_project/src/js/model.js
+++ final_project/src/js/model.js
@@ -68,11 +68,16 @@ export const updateServings = function(newServings){
  state.recipe.servings = newServings; 
 };
 
+const persistBookmarks = function(){
+  localStorage.setItem("bookmarks",JSON.stringify(state.bookmarks));
+}
+
 export const addBookmark = function(recipe){
   state.bookmarks.push(recipe);
   if(recipe.id === state.recipe.id){
     state.recipe.bookmarked = true;
   }
+  persistBookmarks();
 };
 
 export const deleteBookmark = function(id){
@@ -81,4 +86,11 @@ export const deleteBookmark = function(id){
   if(id === state.recipe.id){
     state.recipe.bookmarked = false;
   }
+  persistBookmarks();
 }
+const init = function(){
+  const storage = localStorage.getItem("bookmarks");
+  if(storage) state.bookmarks = JSON.parse(storage);
+  console.log(state.bookmarks);
+}
+init();
